c3bc77ea58ab31c9ee4a22b24e12f95ceb3901b9
Nate Reiners <nate.reiners@gmail.com>
2017-02-06 11:23:36 -0800

    fix profile page background issue

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
-import Splash from './splash/splash';
 import SessionFormContainer from './session_form/session_form_container';
 import App from './app';
 import HomeContainer from './home/home_container';
 import ProfileContainer from './profile/profile_container';
 import PhotoDetailContainer from './photos/photo_detail_container';
 import ProfileFormContainer from './profile/profile_form_container'
+import Splash from './splash/splash';
 
 const Root = ({ store }) => {
 
@@ -26,19 +26,20 @@ const Root = ({ store }) => {
    }
  };
 
+
   return (
     <Provider store={ store }>
       <Router history={ hashHistory }>
-        <Route path="/" component={ App }>
-          <IndexRoute component={ Splash } onEnter={_redirectIfLoggedIn}/>
-          <Route path="login" component={ SessionFormContainer } onEnter={_redirectIfLoggedIn} />
-          <Route path="signup" component={ SessionFormContainer } onEnter={_redirectIfLoggedIn} />
-          <Route path="home" component={ HomeContainer } onEnter={_ensureLoggedIn}/>
-          <Route path="users/:id" component={ProfileContainer} onEnter={_ensureLoggedIn}/>
-          <Route path="users/:id/edit" component={ProfileFormContainer} onEnter={_ensureLoggedIn} />
-          <Route path="profile" component={ProfileContainer} onEnter={_ensureLoggedIn}/>
-          <Route path="photos/:id" component={PhotoDetailContainer} onEnter={_ensureLoggedIn} />
-      </Route>
+        <Route path="/" component={ Splash } />
+          <Route component={ App }>
+            <Route path="login" component={ SessionFormContainer } onEnter={_redirectIfLoggedIn} />
+            <Route path="signup" component={ SessionFormContainer } onEnter={_redirectIfLoggedIn} />
+            <Route path="home" component={ HomeContainer } onEnter={_ensureLoggedIn}/>
+            <Route path="users/:id" component={ProfileContainer} onEnter={_ensureLoggedIn}/>
+            <Route path="users/:id/edit" component={ProfileFormContainer} onEnter={_ensureLoggedIn} />
+            <Route path="profile" component={ProfileContainer} onEnter={_ensureLoggedIn}/>
+            <Route path="photos/:id" component={PhotoDetailContainer} onEnter={_ensureLoggedIn} />
+          </Route>
       </Router>
     </Provider>
   );
